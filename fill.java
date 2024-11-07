3fcafdc5dfeb69849150e5e301eac9a3b30793f7
vishal p <vishalfromvirali@gmail.com>
2024-11-08 01:28:48 +0530

    Add files via upload

---
+++ fill.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class fill{
+    public static void main(String[]args){
+        int[] nums = {2,3,5,6,7};
+        Arrays.fill(nums,80);
+        System.out.println(Arrays.toString(nums));
+        
+    }
+}
